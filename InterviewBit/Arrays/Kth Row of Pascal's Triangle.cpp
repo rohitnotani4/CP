67b36e44e7c58c36ee83c2089398bcb0f60d9f4e
Rohit Notani <rohitnotani4@gmail.com>
2018-05-31 01:19:30 +0530

    Latest solution. AC on 31st May 2018.

--- InterviewBit/Arrays/Kth Row of Pascal's Triangle.cpp
+++ InterviewBit/Arrays/Kth Row of Pascal's Triangle.cpp
@@ -2,50 +2,29 @@
 https://www.interviewbit.com/problems/kth-row-of-pascals-triangle/
 */
 
-void copy_contents (vector<int> & temp, vector<int> & ans)
-{
-    for(int i=0;i<temp.size();i++)
-        ans.push_back(temp[i]);
-    temp.clear();
-}
- 
 vector<int> Solution::getRow(int A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
- 
+
+    vector<int> result,prevRow;
+    if(A>=0)
+        result.push_back(1);
     
-    vector<int> ans,temp;
-    if(A==0)
-        ans.push_back(1);
-    else
-    if(A==1)
+    for(int columnNumber=0;columnNumber<A;columnNumber++)
     {
-        ans.push_back(1);
-        ans.push_back(1);
-    }
-    else
-    {
-        ans.push_back(1);
-        ans.push_back(1);
-        
-        for(int i=2;i<=A;i++)
+        prevRow = result;
+        for(int j=0;j<=columnNumber+1;j++)
         {
-            temp.push_back(1);
-            for(int j=0;j<ans.size()-1;j++)
-            {
-                temp.push_back(ans[j]+ans[j+1]);
-            }
-            temp.push_back(1);
-            
-            ans.clear();
-            
-            copy_contents(temp,ans);
+            if(j==0)
+	            result[j] = prevRow[j];
+	        else 
+	        if(j==(columnNumber+1))
+                result.push_back(prevRow[0]);
+            else
+                result[j] = prevRow[j] + prevRow[j-1];
         }
     }
-                
-	return ans;	
- 
-    
+    return result;
 }
