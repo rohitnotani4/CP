249a415f6836501aa32c65ae7d99202e4cc25a81
Rohit Notani <rohitnotani4@gmail.com>
2018-06-09 00:05:17 +0530

    Latest modified solution. AC on 9th June 2018.

--- InterviewBit/Arrays/Noble Integer.cpp
+++ InterviewBit/Arrays/Noble Integer.cpp
@@ -2,22 +2,16 @@
 https://www.interviewbit.com/problems/noble-integer/
 */
 int Solution::solve(vector<int> &A) {
-    sort(A.begin(),A.end());
     
+    sort(A.begin(),A.end());
     int size = A.size();
     for(int i=0;i<size;i++)
     {
-        int count  = size - i-1;
-        if(i+1 < size && A[i] != A[i+1])
-        {
-            if(A[i] == count)
-                return 1;
-        }
-        else
-        if(i==size-1 && A[i]==0)
-        {
+        while(i+1 < size && A[i]==A[i+1])
+            i++;
+        if(A[i] == size-i-1)
             return 1;
-        }
+        
     }
     return -1;
 }
