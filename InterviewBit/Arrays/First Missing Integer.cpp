76fed120cd4e2f71c66301213ac78290dcd96ed1
Rohit Notani <rohitnotani4@gmail.com>
2018-06-09 20:23:22 +0530

    Another approach. AC on 24th November 2017.

--- InterviewBit/Arrays/First Missing Integer.cpp
+++ InterviewBit/Arrays/First Missing Integer.cpp
@@ -7,18 +7,34 @@ int Solution::firstMissingPositive(vector<int> &A) {
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-     int n = A.size();
-            for (int i = 0; i < n; i++) {
-                if (A[i] > 0 && A[i] <= n) {
-                    int pos = A[i] - 1;
-                    if (A[pos] != A[i]) {
-                        swap(A[pos], A[i]);
-                        i--;
-                    }
-                }
-            }
-            for (int i = 0; i < n; i++) {
-                if (A[i] != i + 1) return (i + 1);
-            }
-            return n + 1;        
+    vector<int> :: iterator it = A.begin();
+    while(it!=A.end()) {
+        if(*it<0) {  
+                A.erase(it);
+        }
+        else {
+            it++;    
+        }
+
+    }
+    it = A.begin();
+    //cout<<"Later: "<<A.size();
+    //disp(A);
+    for(int i=0;i<A.size();i++) {
+        if(abs(A[i])-1<A.size()) { 
+            //cout<<": "<<abs(A[i])-1<<" is less than : "<<A.size()<<endl;
+            A[abs(A[i])-1] = -A[abs(A[i])-1];
+        }       //cout<<"Marking index: "<<abs(A[i])-1<<" as negative!\n";
+    }
+    //disp(A);
+    int c=A.size()+1;
+    for(int i=0;i<A.size();i++) {
+        if(A[i]>0) { 
+            c=i+1;
+            break;
+        }
+    }
+    //cout<<endl;
+    return c;
+
 }
