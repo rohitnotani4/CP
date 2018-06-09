ae9a2396bd99bcf457a70954e7708f5731dd63c2
Rohit Notani <rohitnotani4@gmail.com>
2018-06-09 20:12:21 +0530

    Initial solution. AC on 19th July 2015.

---
+++ InterviewBit/Arrays/First Missing Integer.cpp
@@ -0,0 +1,24 @@
+/*
+https://www.interviewbit.com/problems/first-missing-integer/
+*/
+
+int Solution::firstMissingPositive(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+     int n = A.size();
+            for (int i = 0; i < n; i++) {
+                if (A[i] > 0 && A[i] <= n) {
+                    int pos = A[i] - 1;
+                    if (A[pos] != A[i]) {
+                        swap(A[pos], A[i]);
+                        i--;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                if (A[i] != i + 1) return (i + 1);
+            }
+            return n + 1;        
+}
