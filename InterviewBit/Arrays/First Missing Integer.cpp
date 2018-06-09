416f99264b5ca1c664bf236eac8e3833c68c93ac
Rohit Notani <rohitnotani4@gmail.com>
2018-06-09 22:07:09 +0530

    Latest solution. AC on 9th June 2018.

--- InterviewBit/Arrays/First Missing Integer.cpp
+++ InterviewBit/Arrays/First Missing Integer.cpp
@@ -2,39 +2,63 @@
 https://www.interviewbit.com/problems/first-missing-integer/
 */
 
+// This approach doesn't delete the negative array elements instead it shifts them to the left of array
+// Hence all the calculation for making the array value at index negative involves use of shift value.
+
+void Swap(vector<int> &A, int i, int j)
+{
+    int temp;
+    temp = A[i];
+    A[i] = A[j];
+    A[j] = temp;
+}
+
+int MoveAllNonPositiveToLeftSide(vector<int> &A)
+{
+    int j = 0;
+    for(int i=0;i<A.size();i++)
+    {
+        if(A[i] <= 0)
+        {
+            Swap(A,i,j);
+            j++;
+        }
+    }
+    return j;
+}
+
+int FindFirstMissingPositive(vector<int> &B, int shift)
+{
+    vector<int>::iterator it = B.begin() + shift;
+    
+    int positiveElements = B.size() - shift;
+
+    for(;it!=B.end();it++)
+    {
+        if(abs(*it)-1 < positiveElements) 
+        { 
+            B[abs(*it)+shift-1] = -B[abs(*it)+shift - 1];
+        }
+    }
+
+
+    int j=0;
+    for(it = B.begin() + shift;it!=B.end();it++)
+    {
+        if(*it > 0) 
+            return j+1;
+        j++;
+    }
+    
+    return positiveElements+1;
+}
+
 int Solution::firstMissingPositive(vector<int> &A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    vector<int> :: iterator it = A.begin();
-    while(it!=A.end()) {
-        if(*it<0) {  
-                A.erase(it);
-        }
-        else {
-            it++;    
-        }
-
-    }
-    it = A.begin();
-    //cout<<"Later: "<<A.size();
-    //disp(A);
-    for(int i=0;i<A.size();i++) {
-        if(abs(A[i])-1<A.size()) { 
-            //cout<<": "<<abs(A[i])-1<<" is less than : "<<A.size()<<endl;
-            A[abs(A[i])-1] = -A[abs(A[i])-1];
-        }       //cout<<"Marking index: "<<abs(A[i])-1<<" as negative!\n";
-    }
-    //disp(A);
-    int c=A.size()+1;
-    for(int i=0;i<A.size();i++) {
-        if(A[i]>0) { 
-            c=i+1;
-            break;
-        }
-    }
-    //cout<<endl;
-    return c;
-
+    
+    int shift = MoveAllNonPositiveToLeftSide(A);
+    return FindFirstMissingPositive(A,shift);
 }
