b7c0c26a2a90e2712e0076da24027c4c9eb36bfd
Rohit Notani <rohitnotani4@gmail.com>
2018-06-09 15:05:37 +0530

    Formatted solution. AC on 9th June 2018.

--- InterviewBit/Arrays/Spiral Order Matrix I.cpp
+++ InterviewBit/Arrays/Spiral Order Matrix I.cpp
@@ -2,49 +2,45 @@
 https://www.interviewbit.com/problems/spiral-order-matrix-i/
 */
 
+// Solution: https://www.youtube.com/watch?v=siKFOI8PNKM
 vector<int> Solution::spiralOrder(const vector<vector<int> > &A) {
-    vector<int> result;
     // DO STUFF HERE AND POPULATE result
-    int T,L,R,B,dir,j=0;
-    T=0;
-    L=0;
-    R=A[0].size()-1;
-    B=A.size()-1;
+    vector<int> result;
     
-    dir=0;
+    if(A.empty())
+        return result;
+    int T=0,B=A.size()-1,L=0,R=A[0].size()-1, dir = 0;
     
-    if(A.size()==1 && A[0].size()==1)
-        result.push_back(A[0][0]);
-    else
+    while(T<=B && L<=R)
     {
-        while(T<=B && L<=R)
-            {
-                if(dir==0)
-                {
-                    for(int i=L;i<=R;i++)
-                        result.push_back(A[T][i]);
-                        T++;
-                }
-                else if(dir==1)
-                {
-                    for(int i=T;i<=B;i++)
-                        result.push_back(A[i][R]);
-                        R--;
-                }
-                else if(dir==2)
-                 {
-                    for(int i=R;i>=L;i--)
-                        result.push_back(A[B][i]);
-                        B--;
-                }
-                else if(dir==3)
-                {
-                    for(int i=B;i>=T;i--)
-                        result.push_back(A[i][L]);
-                        L++;
-                }
-                dir = (dir + 1)%4;
-            }
+        if(dir==0) // Traverse Left to Right
+        {
+            for(int j=L;j<=R;j++)
+                result.push_back(A[T][j]);
+            T++;
+        }
+        else
+        if(dir==1) // Traverse Top to Bottom
+        {
+            for(int i=T;i<=B;i++)
+                result.push_back(A[i][R]);
+            R--;
+        }
+        else
+        if(dir==2)
+        {
+            for(int j=R;j>=L;j--)
+                result.push_back(A[B][j]);
+            B--;
+        }
+        else
+        {
+            for(int i=B;i>=T;i--)
+                result.push_back(A[i][L]);
+            L++;    
+        }
+        dir = (dir + 1)%4;
     }
+    
     return result;
 }
