8c5414d117b6c72c46961f0af31c6bd3c638c00b
Rohit Notani <rohitnotani4@gmail.com>
2018-07-10 22:20:09 +0530

    Latest Solution. AC on 10th July 2018.

--- InterviewBit/Strings/Reverse the String.cpp
+++ InterviewBit/Strings/Reverse the String.cpp
@@ -3,64 +3,38 @@ https://www.interviewbit.com/problems/reverse-the-string/
 */
 
 void Solution::reverseWords(string &A) {
-	// Do not write main() function.
-	// Do not read input, instead use the arguments to the function.
-	// Do not print the output, instead return values as specified
-	// Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-	
-	int size = A.size();
-	string reverse;
-	int i=size-1;
-	
-	while(i>=0 && A[i]==' ')
-	{
-		i--;
-	}
- 
-	if(i >= 0)
-	{
-		do
-		{
-			if(A[i]!=' ')
-			{
-				reverse.push_back(A[i]);
-				i--;
-			}
-			else
-			{
-				reverse.push_back(' ');
-				while(i>=0 && A[i]==' ')
-					i--;
-			}    
-		}while(i>=0);
-	}
-	
-	A.clear();
-	if(!reverse.empty())
-	{
-		int j=0;
-		if(reverse[reverse.size()-1] == ' ')
-			j++;
-		
-		string temp;    
-		for(;j<reverse.size();j++)
-		{
-			if(reverse[j]!=' ')
-			{
-				temp.push_back(reverse[j]);    
-			}
-			else
-			{
-				for(int k=temp.size()-1;k>=0;k--)
-					A.push_back(temp[k]);
-				A.push_back(' ');
-				temp.clear();
-			}
-		}
-		if(temp.size()>0)
-		{
-			for(int k=temp.size()-1;k>=0;k--)
-					A.push_back(temp[k]);
-		}			
-	}
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    vector<string> words;
+    string word;
+    int index=A.size()-1;
+    while(index >= 0)
+    {
+        if(A[index]==' ')
+        {
+            if(word.size())
+            {
+                words.push_back(word);
+                word.clear();
+            }
+        }
+        else
+        {
+            word.push_back(A[index]);
+        }
+        index--;
+    }
+    words.push_back(word); // Last word
+    A.clear();
+    for(int wordIndex=0;wordIndex<words.size();wordIndex++)
+    {
+        for(int letterIndex=words[wordIndex].size()-1;letterIndex>=0;letterIndex--)
+            A.push_back(words[wordIndex][letterIndex]);
+        if (wordIndex != words.size()-1)
+            A += " ";
+    }
+    return ;
 }
