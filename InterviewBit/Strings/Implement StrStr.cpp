f76c963647f201c32b6a0401bdfcc232685393f7
Rohit Notani <rohitnotani4@gmail.com>
2018-07-17 00:22:13 +0530

    Latest solution. AC on 17th July 2018.
    Used the KMP method as described in Tushar Roy's videos.

--- InterviewBit/Strings/Implement StrStr.cpp
+++ InterviewBit/Strings/Implement StrStr.cpp
@@ -2,67 +2,70 @@
 https://www.interviewbit.com/problems/implement-strstr/
 */
 
+vector<int> computeLPSTable(string& pattern, int patternSize)
+{
+    vector<int> LPS(patternSize,0);    
+    int index = 1, len = 0;
+    while(index < patternSize)
+    {
+        if(pattern[index] == pattern[len])
+        {
+            len++;
+            LPS[index] = len;
+            index++;
+        }
+        else
+        {
+            if(len > 0)
+            {
+                len = LPS[len-1];
+            }
+            else
+            {
+                len = 0;
+                index++;
+            }
+        }
+    }
+    return LPS;
+}
+
+int KMP(string& haystack, string& needle)
+{
+    vector<int> LPS; 
+    LPS = computeLPSTable(needle, needle.size());
+    
+    int  sIndex=0, pIndex = 0;
+    while(sIndex < haystack.size() && pIndex < needle.size())
+    {
+        if(haystack[sIndex] == needle[pIndex])
+        {
+            sIndex++;
+            pIndex++;
+        }
+        else
+        {
+            if(pIndex != 0)
+                pIndex = LPS[pIndex-1];
+            else
+                sIndex++;
+        }
+    }
+    
+    if(pIndex == needle.size())
+        return sIndex-pIndex; // Since we have to return start of string Index 
+
+    return -1;
+}
+
 int Solution::strStr(string haystack, string needle) {
         // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
-    
-    int patternSize = needle.size();
-    int stringSize = haystack.size();
-    vector<int> matchTable(patternSize,0);
-    if(needle.empty() || haystack.empty() || (patternSize > stringSize))
+    if(needle.empty() || haystack.empty() || (needle.size() > haystack.size()))
         return -1;
-    
-    
-    //aba   
-    matchTable.push_back(0);
-    // cout<<"0 ";
-    for(int i=1;i<patternSize;i++)
-    {
-        int pos = 0;
-        if(needle[i] == needle[pos + matchTable[i-1]])
-            matchTable[i] = matchTable[i-1] + 1;
-        else
-            matchTable[i] = 0;
-        // cout<<matchTable[i]<<" ";
-    }    
-    
-    int i = 0, j =0,start=-1,partialMatchLength = 0,flag = 0;
-    while(i<stringSize)
-    {
-        if(haystack[i] == needle[j])
-        {
-            if(j==0)
-                start = i;
-            // cout<<"i = "<<i<<" and j= "<<j<<"\n";
-            j++;
-            i++;
-            partialMatchLength++;
-            if(j==patternSize)
-            {
-                flag=1;
-                break;
-            }
-            
-        }
-        else
-        {
-            if((partialMatchLength - 1) >=0)
-            {
-                i = start + (partialMatchLength - matchTable[partialMatchLength - 1]);
-            }
-            else
-                i++;
-            j=0;
-            partialMatchLength = 0;
-            start = -1;
-            // cout<<"i = "<<i<<"\n";
-        }
-    }
-    
-    if(!flag)
-        start = -1;
-    return start;   
+        
+    return KMP(haystack, needle);
 }
