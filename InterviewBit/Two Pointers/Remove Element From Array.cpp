5b2e834aee35d7e645ba2e2b1bb015435a879f83
Rohit Notani <rohitnotani4@gmail.com>
2018-07-28 20:39:04 +0530

    Initial solution. AC on 28th July 2018.

---
+++ InterviewBit/Two Pointers/Remove Element From Array.cpp
@@ -0,0 +1,25 @@
+/*
+https://www.interviewbit.com/problems/remove-element-from-array/
+*/
+
+
+
+int Solution::removeElement(vector<int> &A, int B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    int j = A.size();
+    int begin=0;
+    for(int i=0;i<A.size();i++) 
+        if(A[i]!=B) 
+            A[begin++]=A[i];
+
+    while(j > begin)
+    {
+        A.pop_back();
+        j--;
+    }
+    return A.size();
+}
