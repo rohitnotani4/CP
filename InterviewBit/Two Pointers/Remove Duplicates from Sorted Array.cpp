bd24775002cadc4c2462eca9474101913e54f9f6
Rohit Notani <rohitnotani4@gmail.com>
2018-07-29 14:17:38 +0530

    Latest solution. AC on 29th July 2018.
    Deleted the popping code. Simplified the while loop.

--- InterviewBit/Two Pointers/Remove Duplicates from Sorted Array.cpp
+++ InterviewBit/Two Pointers/Remove Duplicates from Sorted Array.cpp
@@ -8,31 +8,20 @@ int Solution::removeDuplicates(vector<int> &A) {
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
-    // 1, 2, 2, 2, 2, 3, 4, 4, 4, 5, 5
     int size = A.size();
-    
     if(size <= 1)
         return size;
     
     int prevIndex = 0, currIndex = 1, uniqueElements = 1;
     while(currIndex < size)
     {
-        if(A[currIndex] == A[prevIndex])
-        {
-            currIndex++;
-        }
-        else
+        if(A[currIndex] != A[prevIndex])
         {
             A[uniqueElements] = A[currIndex];
             prevIndex = currIndex;
             uniqueElements++;
-            currIndex++;
         }
-    }
-    
-    while(uniqueElements <= A.size())
-    {
-        A.pop_back();
+        currIndex++;
     }
     
     return uniqueElements;
