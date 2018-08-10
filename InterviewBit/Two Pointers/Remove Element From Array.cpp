81a0ed4fb923efbb31db2d64460b840d9412eeec
Rohit Notani <rohitnotani4@gmail.com>
2018-08-11 00:57:23 +0530

    Removed extra lines

--- InterviewBit/Two Pointers/Remove Element From Array.cpp
+++ InterviewBit/Two Pointers/Remove Element From Array.cpp
@@ -2,8 +2,6 @@
 https://www.interviewbit.com/problems/remove-element-from-array/
 */
 
-
-
 int Solution::removeElement(vector<int> &A, int B) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
