ecf000eecf7268abfb67e357a7914328b9b75d69
Rohit Notani <rohitnotani4@gmail.com>
2018-08-11 01:00:18 +0530

    Initial solution. AC on 9th August 2018.
    Refer  :
    https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/discuss/27970/Share-my-O(N)-time-and-O(1)-solution-when-duplicates-are-allowed-at-most-K-times

---
+++ InterviewBit/Two Pointers/Remove Duplicates From Sorted Array II.cpp
@@ -0,0 +1,33 @@
+/*
+https://www.interviewbit.com/problems/remove-duplicates-from-sorted-array-ii/
+*/
+
+int Solution::removeDuplicates(vector<int> &A) 
+{
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    int arraySize = A.size();
+    if(arraySize <= 2)
+        return arraySize;
+        
+    int count = 1, currentIndex = 1, i =1, duplicateCount = 2;      
+    while(i < arraySize)
+    {
+        if(A[i] != A[i-1])
+        {
+            count = 1;
+            A[currentIndex++] = A[i];
+        }
+        else
+        if(count < duplicateCount)
+        {
+            A[currentIndex++] = A[i];
+            count++;
+        }
+        i++;
+    }
+    return currentIndex;
+}
