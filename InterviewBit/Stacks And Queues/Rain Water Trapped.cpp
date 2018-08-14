c9b570fd4f3309f60e199473b7935347757471e2
Rohit Notani <rohitnotani4@gmail.com>
2018-08-14 22:15:38 +0530

    Latest solution. AC on 14th August 2018.
    Uses O(n) extra space compared to optimized solution which uses constant space.

--- InterviewBit/Stacks And Queues/Rain Water Trapped.cpp
+++ InterviewBit/Stacks And Queues/Rain Water Trapped.cpp
@@ -7,26 +7,26 @@ int Solution::trap(const vector<int> &A) {
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-
-    if(A.size() < 1)
+    
+    if(A.size() <= 1)
         return 0;
-    vector<int> height;
-    height = A;
-
-    int a = 0, b = height.size() - 1, maxLeft = 0, maxRight = 0, res = 0;
-    while (a <= b) 
+    
+    // For each index find what it is the maximum height on right side
+    vector<int> maxHeightOnRight(A.size(), 0);
+    int currMax = INT_MIN;
+    for(int i=A.size()-1;i>=0;i--)
+        currMax = maxHeightOnRight[i] = max(currMax,A[i]);
+    
+    // We aren't storing maxOfLeft in array because we can use the maxHeightOnLeft
+    // for all indexes when we move from 0 to array length
+    int maxHeightOnLeft = INT_MIN;
+    int waterTrapped = 0, totalWaterTrapped = 0;
+    for(int i=0;i<A.size();i++)
     {
-        maxLeft = max(maxLeft, height[a]);
-        maxRight = max(maxRight, height[b]);
-        if (maxLeft <= maxRight) 
-        {
-            res += maxLeft - height[a];
-            a++;
-        } else 
-        {
-            res += maxRight - height[b];
-            b--;
-        }
+        maxHeightOnLeft = max(maxHeightOnLeft,A[i]);
+            
+        waterTrapped = min(maxHeightOnLeft, maxHeightOnRight[i]) - A[i];
+        totalWaterTrapped += waterTrapped;
     }
-    return res;
+    return totalWaterTrapped;
 }
