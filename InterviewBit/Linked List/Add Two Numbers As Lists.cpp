cba07dde00f656b417e99e1624684a4a8c0763b2
Rohit Notani <rohitnotani4@gmail.com>
2018-08-19 15:05:47 +0530

    Latest solution. AC on 19th August 2018.
    Simplified the initial solution.

--- InterviewBit/Linked List/Add Two Numbers As Lists.cpp
+++ InterviewBit/Linked List/Add Two Numbers As Lists.cpp
@@ -26,78 +26,41 @@ https://www.interviewbit.com/problems/add-two-numbers-as-lists/
  */
 listnode* addTwoNumbers(listnode* A, listnode* B) 
 {
-    listnode* head = listnode_new(0);
-    listnode* temp = head;
     
-    unsigned long long firstSum =0,secondSum=0,totalSum=0;
-    
-    if(A==NULL && B==NULL)
-        return A;
-    
-    int carry=0;
-    while(A!=NULL && B!=NULL)
+    listnode* curr = listnode_new(0);
+    listnode* head = curr;
+
+    int carry = 0;
+    while(A != NULL && B!= NULL)
     {
-        totalSum = ( (unsigned long long)(A->val) +  (unsigned long long)(B->val)) + carry;
-        if(totalSum >= 10)
-        {
-            carry = 1;
-            totalSum %= 10;
-        }
-        else
-            carry = 0;
-            
-        // printf("Total Sum = %d\n",totalSum);
-        listnode* newNode = listnode_new(totalSum);
-        temp->next = newNode;
-        temp = temp->next;
+        int sum = A->val + B->val + carry;
+        carry = sum/10;
+        curr->next = listnode_new(sum%10);
+        curr = curr->next;
         A = A->next;
         B = B->next;
     }
     
-    
-    while(A!=NULL)
+    while(A != NULL)
     {
-        totalSum = ( (unsigned long long)(A->val) + carry);
-        if(totalSum >= 10)
-        {
-            carry = 1;
-            totalSum %= 10;
-        }
-        else
-            carry = 0;
-        
-        // printf("Total Sum = %d\n",totalSum);
-        listnode* newNode = listnode_new(totalSum);
-        temp->next = newNode;
-        temp = temp->next;
-        
+        int sum = A->val + carry;
+        carry = sum/10;
+        curr->next = listnode_new(sum%10);
+        curr = curr->next;
         A = A->next;
     }
     
-    while(B!=NULL)
+    while(B != NULL)
     {
-        totalSum = ( (unsigned long long)(B->val) + carry);
-        if(totalSum >= 10)
-        {
-            carry = 1;
-            totalSum %= 10;
-        }
-        else
-            carry = 0;
-        
-        // printf("Total Sum = %d\n",totalSum);
-        listnode* newNode = listnode_new(totalSum);
-        temp->next = newNode;
-        temp = temp->next;
-        
+        int sum = B->val + carry;
+        carry = sum/10;
+        curr->next = listnode_new(sum%10);
+        curr = curr->next;
         B = B->next;
     }
     
-    if(carry > 0)
-    {
-        listnode* newNode = listnode_new(carry);
-        temp->next = newNode;
-        temp = temp->next;
-    }    
+    if(carry)
+        curr->next = listnode_new(carry);
+        
     return head->next;
 }
