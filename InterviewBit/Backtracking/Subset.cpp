ca202f48aa81b3fe7ea396009eef65b3b3625027
Rohit Notani <rohitnotani4@gmail.com>
2018-12-18 23:05:05 +0530

    Latest solution. AC on 18th December 2018.
    Updated to make it same as Subset II

--- InterviewBit/Backtracking/Subset.cpp
+++ InterviewBit/Backtracking/Subset.cpp
@@ -4,11 +4,11 @@ https://www.interviewbit.com/problems/subset/
 
 void makeSet(vector<int>& nums, vector<int>& currSubset, vector<vector<int>>& ans, int curr)
 {
+    ans.push_back(currSubset);
     for(int index = curr;index < nums.size(); index++)
     {
         currSubset.push_back(nums[index]);
-        ans.push_back(currSubset);
-        
+
         makeSet(nums, currSubset, ans, index + 1);
         
         currSubset.pop_back();
@@ -20,10 +20,7 @@ vector<vector<int> > Solution::subsets(vector<int> &A)
 {
     vector<int> currSubset;
     vector<vector<int> > ans;
-    ans.push_back(currSubset);
-    if(A.size() < 1)
-        return ans;
-    
+
     sort(A.begin(),A.end());
     
     makeSet(A, currSubset, ans, 0);
