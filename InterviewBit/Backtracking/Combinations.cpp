65a8fe63a29ce2c14eacdfb7ae206c8f636ab7e7
Rohit Notani <rohitnotani4@gmail.com>
2018-12-20 21:05:58 +0530

    Latest solution. AC on 20th October 2018.
    Simplified the initial program.

--- InterviewBit/Backtracking/Combinations.cpp
+++ InterviewBit/Backtracking/Combinations.cpp
@@ -2,33 +2,32 @@
 https://www.interviewbit.com/problems/combinations/
 */
 
-bool calculateRecursive(vector<vector<int> >& ans,vector<int>& temp, int n, int currentIndex,int k)
+void combinationsHelper(vector<vector<int> >& result,vector<int>& chosen, int n,int currNumber,int k)
 {
-    if(temp.size()==k)
+    if(chosen.size()==k)
     {
-        return true;
+        result.push_back(chosen);
     }
-    else if(temp.size() < k && k<=n)
+    else
     {
-        for(int j=currentIndex;j<=n;j++)   
+        for(int i=currNumber;i<=n;i++)
         {
-            temp.push_back(j);
-            if(calculateRecursive(ans,temp,n,j+1,k))
-                ans.push_back(temp);
-            temp.pop_back();
+            chosen.push_back(i);
+            
+            combinationsHelper(result,chosen,n,i+1,k);
+            
+            chosen.pop_back();
         }
     }
-    return false;
 }
-
-
+ 
 vector<vector<int> > Solution::combine(int n, int k) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    vector<int> temp;
-    vector<vector<int> > ans;
-    calculateRecursive(ans,temp,n,1,k);
-    return ans;   
+    vector<int> chosen;
+    vector<vector<int> > result;
+    combinationsHelper(result,chosen,n,1,k);
+    return result;   
 }
