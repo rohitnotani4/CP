55622bf1a160c32852ae4f404fbcfa810da200d0
Rohit Notani <rohitnotani4@gmail.com>
2018-12-21 00:55:28 +0530

    Latest solution. AC on 21st October 2018.
    Simplified the initial solution.

--- InterviewBit/Backtracking/Combination Sum II.cpp
+++ InterviewBit/Backtracking/Combination Sum II.cpp
@@ -2,64 +2,55 @@
 https://www.interviewbit.com/problems/combination-sum-ii/
 */
 
-bool checkifSame(vector<vector<int> >& ans, vector<int>& temp)
+bool checkIfExists(vector<vector<int> >& result, vector<int>& chosen)
 {
-    int prevSoultion = ans.size()-1;
+    int lastResultIndex = result.size()-1;
     
-    if(ans.size()==0)
+    if(result.size()==0 || result[lastResultIndex].size() != chosen.size())
         return false;
         
-    if(ans[prevSoultion].size() != temp.size())
-        return false;
-    
-    for(int j=0;j<ans[prevSoultion].size();j++)
+    for(int j=chosen.size()-1;j>=0;j--)
     {
-        if(ans[prevSoultion][j]!=temp[j])
+        if(result[lastResultIndex][j]!=chosen[j])
             return false;
     }
     return true;
 }
- 
-bool calculateSumRecursively 
-(vector<vector<int> >& ans,vector<int>& temp, vector<int>& A, int sum,int target, int currentIndex)
+
+void combinationSumHelper(vector<vector<int> >& result, vector<int>& chosen, vector<int>& input, int index, int currentSum, int target)
 {
-    if(sum==target)
+    if(currentSum > target)
     {
-        return true;
+        return;    
     }
     
-    if(sum < target)
+    if(currentSum == target)
     {
-        for(int i=currentIndex;i<A.size();i++)   
-        {
-            // *prev = (*it);
-            temp.push_back(A[i]);
-            sum += A[i];
-            if(calculateSumRecursively(ans,temp,A,sum,target,i+1))
-            {
-                if(!checkifSame(ans,temp))
-                    ans.push_back(temp);
-            }
-            temp.pop_back();
-            sum -= A[i];
-        }
+        if(!checkIfExists(result, chosen))
+            result.push_back(chosen);
+        return ;
     }
-    return false;
+    
+    for(int i = index;i<input.size();i++)
+    {
+        chosen.push_back(input[i]);
+        currentSum += input[i];
+        
+        combinationSumHelper(result, chosen, input, i+1, currentSum, target);
+        
+        chosen.pop_back();
+        currentSum -= input[i];
+    }
 }
- 
- 
 vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
-    int prev = -1;
-    vector<int> temp;
-    vector<vector<int> > ans;
-    
+    vector<int> chosen;
+    vector<vector<int> > result;
     sort(A.begin(),A.end());
-        
-    calculateSumRecursively(ans,temp,A,0,B,0);
-    return ans;  
+    combinationSumHelper(result,chosen,A,0,0,B);
+    return result;  
 }
