bcf7f5ee3e992835514c0eb574cb1088518d96cb
Rohit Notani <rohitnotani4@gmail.com>
2018-12-23 12:31:56 +0530

    Latest solution. AC on 23rd December 2018.
    Simplified existing solution.

--- InterviewBit/Backtracking/NQueens.cpp
+++ InterviewBit/Backtracking/NQueens.cpp
@@ -5,7 +5,6 @@ https://www.interviewbit.com/problems/nqueens/
 vector<string> AddToAnswer(vector<pair<int,int> >& placedQueens,int A)
 {
     vector<string> oneSolution;
-    
     for(int i=0;i<placedQueens.size();i++)
     {
         string temp (A, '.');
@@ -14,60 +13,52 @@ vector<string> AddToAnswer(vector<pair<int,int> >& placedQueens,int A)
     }
     return oneSolution;
 }
- 
+
 bool canPlace(pair<int,int> currentPos, vector<pair<int,int> >& placedQueens)
 {
     for(int i=0;i<placedQueens.size();i++)
     {
         if(currentPos.second == placedQueens[i].second
             || ( (currentPos.first + currentPos.second) == 
-            (placedQueens[i].first + placedQueens[i].second))
+               (placedQueens[i].first + placedQueens[i].second))
             || ( (currentPos.first - currentPos.second) == 
-            (placedQueens[i].first - placedQueens[i].second))
+               (placedQueens[i].first - placedQueens[i].second))
             )
                 return false;
     }
     return true;
 }
- 
-bool getSolution(int row, vector<pair<int,int> >& placedQueens, vector<vector<string> >& ans, int A)
+
+void getSolution(vector<vector<string> >& result, vector<pair<int,int> >& placedQueens, int currRow, int N)
 {
-    if(row == A)
-        return true;
- 
-    for(int j=0;j<A;j++)
+    if(currRow == N)
     {
-        bool place = false;
-        pair<int,int> currenPos = make_pair(row,j);
-        
-        place = canPlace(currenPos,placedQueens);
-        if(place)
+        result.push_back(AddToAnswer(placedQueens, N));
+        return ;
+    }
+
+    for(int j=0;j<N;j++)
+    {
+        pair<int,int> currenPos = make_pair(currRow, j);
+        if(canPlace(currenPos, placedQueens))
         {
-            // cout<<"Position "<<currenPos.first<<","<<currenPos.second<<"\n";
             placedQueens.push_back(currenPos);
-            if(getSolution(row+1,placedQueens,ans,A))
-            {
-                // cout<<"We have a solution"<<"\n";
-                ans.push_back(AddToAnswer(placedQueens,A));
-            }
+            
+            getSolution(result, placedQueens, currRow+1, N);
+            
             placedQueens.pop_back();    
         }
     }
-    
-    return false;
 }
- 
+
 vector<vector<string> > Solution::solveNQueens(int A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
-    vector<vector<string> > ans;
-    vector<pair<int,int> >queenPosition;
-    int row = 0;
-    getSolution(0,queenPosition,ans,A);
-    
-    // cout<<"No of solutions = "<<ans.size()<<"\n";
-    return ans;
+    vector<vector<string> > result;
+    vector<pair<int,int> > queenPosition;
+    getSolution(result, queenPosition, 0, A);
+    return result;
 }
