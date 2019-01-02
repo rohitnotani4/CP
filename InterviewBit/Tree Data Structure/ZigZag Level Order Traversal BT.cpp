2c5593e936181ba008a0df9f633722999a13bd11
Rohit Notani <rohitnotani4@gmail.com>
2019-01-02 21:26:48 +0530

    Latest solution. AC on 30th December 2018.
    Used Level order traversal with one additional change resulting in simplified solution.

--- InterviewBit/Tree Data Structure/ZigZag Level Order Traversal BT.cpp
+++ InterviewBit/Tree Data Structure/ZigZag Level Order Traversal BT.cpp
@@ -11,69 +11,40 @@ https://www.interviewbit.com/problems/zigzag-level-order-traversal-bt/
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- 
-void zigzagDFS(TreeNode* root, vector<vector<int> >& ans)
-{
-    int leftToRight = 0;
-    TreeNode* curr;
-    vector<int> temp;
-    stack<TreeNode*> left, right;
-    left.push(root);
-    // currLevel.push(NULL);
-    
-    while(!left.empty() || !right.empty())
-    {
-        while(!left.empty())
-        {
-            curr = left.top();
-            left.pop();
-            if(curr->left != NULL)
-                right.push(curr->left);
-            if(curr->right != NULL)
-                right.push(curr->right);
-                
-            temp.push_back(curr->val);
-        }
-        
-        if(left.empty())
-        {
-            if(temp.size())
-                ans.push_back(temp);
-            temp.clear();
-        }
-        
-        while(!right.empty())
-        {
-            curr = right.top();
-            right.pop();
-            if(curr->right != NULL)
-                left.push(curr->right);
-            if(curr->left != NULL)
-                left.push(curr->left);    
-                
-                
-            temp.push_back(curr->val);
-        }
-        
-        if(right.empty())
-        {
-            if(temp.size())
-                ans.push_back(temp);
-            temp.clear();
-        }
-    }
-    
-}
- 
 vector<vector<int> > Solution::zigzagLevelOrder(TreeNode* A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
-    vector<vector<int> > ans;
-    if(A != NULL)
-        zigzagDFS(A,ans);
-    
-    return ans;
+    vector<vector<int> > result;
+    if (A == NULL)
+        return result;
+    queue<TreeNode*> nodes;
+    nodes.push(A);
+    while(!nodes.empty())
+    {
+        vector<int> level;
+        int size = nodes.size();
+        while(size > 0)
+        {
+            TreeNode* curr = nodes.front();
+            nodes.pop();
+            level.push_back(curr->val);
+            
+            if(curr->left != NULL)
+                nodes.push(curr->left);
+            
+            if(curr->right != NULL)
+                nodes.push(curr->right);
+            
+            size--;
+        }
+        if (result.size()%2)
+        {
+            reverse(level.begin(), level.end());
+        }
+        result.push_back(level);         
+    }
+    return result;
 }
