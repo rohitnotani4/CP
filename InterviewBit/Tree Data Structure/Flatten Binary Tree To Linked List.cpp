bc0b0d27dafca060d1cedbd5f79e3ecf17523627
Rohit Notani <rohitnotani4@gmail.com>
2019-01-26 19:53:43 +0530

    Latest solution. AC on 26th January 2019.
    Simple NON-recursive solution, O(1) space complexity!
    
    Link: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/discuss/37010/Share-my-simple-NON-recursive-solution-O(1)-space-complexity!

--- InterviewBit/Tree Data Structure/Flatten Binary Tree To Linked List.cpp
+++ InterviewBit/Tree Data Structure/Flatten Binary Tree To Linked List.cpp
@@ -11,33 +11,37 @@ https://www.interviewbit.com/problems/flatten-binary-tree-to-linked-list/
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- 
-void preOrderTraversal(TreeNode* root, vector<TreeNode*>& nodeList) 
-{
-    if(root == NULL)
-        return ;
-    
-    nodeList.push_back(root);
-    preOrderTraversal(root->left, nodeList);
-    preOrderTraversal(root->right, nodeList);
-}
-
 TreeNode* Solution::flatten(TreeNode* A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
-    vector<TreeNode*> nodeList;
-    TreeNode* root = A;
-    preOrderTraversal(A,nodeList);
-    
-    root->left = NULL;
-    for(int i=0;i<nodeList.size()-1;i++)
+    TreeNode* now = A;
+    while(now != NULL)
     {
-        nodeList[i]->right = nodeList[i+1];
-        nodeList[i]->left = NULL;
+        if (now->left != NULL)
+        {
+            TreeNode* pre = now->left;
+            // Find the rightmost node of this node
+            while(pre->right != NULL)
+            {
+                pre = pre->right;
+            }
+            // Point to right of this rightmost node to now->right. In other words this 
+            // sets the right pointer of the last node in the left subtree.
+            pre->right = now->right;
+            // Now set the start of left subtree to now's right.
+            now->right = now->left;
+            // Set the left of now to NULL
+            now->left = NULL;
+        }
+        else
+        {
+            // There is nothing on left subtree, move to right subtree
+            now = now->right;
+        }
     }
-    
-    return root;
+    return A;
 }
+
