31b17b0d49836bfbd4c84c61012b80a88308622d
Rohit Notani <rohitnotani4@gmail.com>
2019-01-29 23:32:50 +0530

    Latest solution. AC on 29th January 2019.
    Named the variables better and simplified a bit.

--- InterviewBit/Dynamic Programming/Length Of Longest Subsequence.cpp
+++ InterviewBit/Dynamic Programming/Length Of Longest Subsequence.cpp
@@ -5,26 +5,31 @@ https://www.interviewbit.com/problems/length-of-longest-subsequence/
 int Solution::longestSubsequenceLength(const vector<int> &A) 
 {
     int n=A.size();
-    int lis[n];
-    int lds[n];
-    for(int i=0;i<n;i++){
-     lis[i]=1;
-     lds[i]=1;
-    }
-    for(int i=1;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(A[i]>A[j]&&lis[i]<lis[j]+1)
-             lis[i]=lis[j]+1;
+    vector<int> lis (n,1);
+    vector<int> lds (n,1);
+    
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if (A[j] < A[i] && lis[i] < (lis[j] + 1))
+                lis[i] = lis[j] + 1;
         }
     }
-    for(int i=n-2;i>=0;i--){
-        for(int j=n-1;j>i;j--){
-            if(A[i]>A[j]&&lds[i]<lds[j]+1)
-             lds[i]=lds[j]+1;
+    
+    for(int i=n-2;i>=0;i--)
+    {
+        for(int j=n-1;j>i;j--)
+        {
+            if (A[j] < A[i] && lds[i] < (lds[j] + 1))
+                lds[i] = lds[j] + 1;
         }
     }
-    int ans=0;
+    
+    int maxLength = 0;
     for(int i=0;i<n;i++)
-     ans=max(ans,lis[i]+lds[i]-1);
-    return ans;
+    {
+        maxLength = max(maxLength, lis[i] + lds[i] - 1);
+    }
+    return maxLength;
 }
