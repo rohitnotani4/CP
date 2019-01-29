5f29ea94823bf8c36e832afb3255c2c12eb5e688
Rohit Notani <rohitnotani4@gmail.com>
2019-01-29 20:46:33 +0530

    Latest solution. AC on 29th January 2019.
    Simplified the initial solution.

--- InterviewBit/Dynamic Programming/Stairs.cpp
+++ InterviewBit/Dynamic Programming/Stairs.cpp
@@ -8,15 +8,16 @@ int Solution::climbStairs(int A)
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    int dp[A+1];
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 2;
-    for(int i=3;i<=A;i++)
-    {
+    
+    if (A <= 0)
+        return 0;
         
+    int dp[A];
+    dp[0] = 1;
+    dp[1] = 2;
+    for(int i=2;i<A;i++)
+    {
         dp[i] = dp[i-2] + dp[i-1];
     }
-    
-    return dp[A];
+    return dp[A-1];
 }
