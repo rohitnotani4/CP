9e09c0d6dbbf6bb1f3fbc6126098d936c836f1a9
Rohit Notani <rohitnotani4@gmail.com>
2019-02-20 21:31:20 +0530

    Latest solution. AC on 20th February 2019.
    Simplified the initial solution.

--- InterviewBit/Dynamic Programming/Edit Distance.cpp
+++ InterviewBit/Dynamic Programming/Edit Distance.cpp
@@ -2,20 +2,13 @@
 https://www.interviewbit.com/problems/edit-distance/
 */
 
-int findMin(int x,int y,int z)
-{
-    return min(min(x,y),z);
-}
- 
 int Solution::minDistance(string A, string B) 
 {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    int ans = 0;
     int editDistance[A.size()+1][B.size()+1];
-    
     for(int i=0;i<=A.size();i++)
         editDistance[i][0] = i;
     
@@ -29,12 +22,8 @@ int Solution::minDistance(string A, string B)
             if(A[i-1]==B[j-1])
                 editDistance[i][j] = editDistance[i-1][j-1];
             else
-                editDistance[i][j] = 1 + findMin(editDistance[i][j-1],
-                                                 editDistance[i-1][j],
-                                                 editDistance[i-1][j-1]);
-        }                                           
+                editDistance[i][j] = 1 + min(editDistance[i][j-1], min(editDistance[i-1][j], editDistance[i-1][j-1]));        }                                           
     }
     
-    ans = editDistance[A.size()][B.size()];
-    return ans;
+    return editDistance[A.size()][B.size()];
 }
