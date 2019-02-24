204ec317b33602a7bdac78c1d09a1eb980826951
Rohit Notani <rohitnotani4@gmail.com>
2019-02-24 15:16:02 +0530

    Latest solution. AC on 24th Feb 2019.
    Simplified the initial solution and added comments.

--- InterviewBit/Dynamic Programming/Regular Expression II.cpp
+++ InterviewBit/Dynamic Programming/Regular Expression II.cpp
@@ -2,38 +2,42 @@
 https://www.interviewbit.com/problems/regular-expression-ii/
 */
 
-int Solution::isMatch(const string &s, const string &p) {
+int Solution::isMatch(const string s, const string p) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
-    int textSize, patternSize;
-    
-    textSize = s.empty() ? 0 : s.size();
-    patternSize = p.empty() ? 0 : p.size();
+    int textSize = s.size();
+    int patternSize = p.size();
     
     vector<vector<bool> > dp(textSize+1, vector<bool>(patternSize+1));
-    // int dp[textSize+1][patternSize+1];
+
+    // An empty string would match with empty pattern so dp[0][0] = 'T'
     dp[0][0] = true;
     
+    // An empty pattern wouldn't match with any text having length > 0. 
+    // So T[i][0] = 'F' for all i > 0 and i < length of text
     for(int i=1;i<=textSize;i++)
         dp[i][0] = false;
     
+    // A non empty pattern will match with an empty text only if the pattern is of type a*, a*b* etc.
     for(int j=1;j<=patternSize;j++)
     {
-        if(p[j-1]=='*')
+        if(p[j-1]=='*' && j-2 >= 0)
             dp[0][j] = dp[0][j-2];
         else
             dp[0][j] = false;
     }
     
-    for(int i=1;i<=textSize;i++)
+    for(int i=1;i<dp.size();i++)
     {
-        for(int j=1;j<=patternSize;j++)
+        for(int j=1;j<dp[i].size();j++)
         {
             if(p[j-1]=='.' || p[j-1] == s[i-1])
+            {
                 dp[i][j] = dp[i-1][j-1];
+            }
             else
             if(p[j-1]=='*')
             {
@@ -42,9 +46,11 @@ int Solution::isMatch(const string &s, const string &p) {
                     dp[i][j] = dp[i][j] || dp[i-1][j];
             }
             else
+            {
                 dp[i][j] = false;
+            }
+                
         }
     }
-    
     return dp[textSize][patternSize];
 }
