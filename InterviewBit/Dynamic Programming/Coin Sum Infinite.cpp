cb6b8f35c60cb522ced32853ab5ed46b67c9a5f5
Rohit Notani <rohitnotani4@gmail.com>
2019-03-06 23:25:30 +0530

    Latest solution. AC on 6th March 2019.
    Used 2-D approach based on 
    https://www.youtube.com/watch?v=DJ4a7cmjZY0&t

--- InterviewBit/Dynamic Programming/Coin Sum Infinite.cpp
+++ InterviewBit/Dynamic Programming/Coin Sum Infinite.cpp
@@ -4,21 +4,37 @@ https://www.interviewbit.com/problems/coin-sum-infinite/
 
 int Solution::coinchange2(vector<int> &A, int B) 
 {
-    if(B==0)
-        return 1;
+    vector<vector<int>> dp (2, vector<int> (B+1, 0));
     
-    if(A.size() < 1)
-        return 0;
-    
-    vector<int> dp(B+1,0);
-    dp[0] = 1;
-    
-    for(int i=0;i<A.size();i++)
+    dp[0][0] = 1;
+    for (int i = 1; i <= A.size(); i++) 
     {
-        for(int denomination = A[i];denomination <= B; denomination++)
+        /*
+          Set the subproblem for the amount of 0 to 1 when
+          solving this row
+        */
+        dp[1][0] = 1;
+        for (int j = 1; j <= B; j++) 
         {
-            dp[denomination] = (dp[denomination]%1000007 + dp[denomination - A[i]]%1000007)%1000007;
+            int currentCoinValue = A[i-1];
+            /*
+            dp[i][j] will be the sum of the ways to make change not considering
+            this coin (dp[i-1][j]) and the ways to make change considering this
+            coin (dp[i][j] - currentCoinValue] ONLY if currentCoinValue <= j, otherwise
+            this coin can not contribute to the total # of ways to make change at this
+            sub problem target amount)
+            */
+            int withoutThisCoin = dp[0][j];
+            int withThisCoin = currentCoinValue <= j ? dp[1][j - currentCoinValue] : 0;
+    
+            dp[1][j] = (withoutThisCoin% 1000007 + withThisCoin% 1000007)% 1000007;
+        }
+        
+        for(int j = 1; j <= B; j++)
+        {
+            dp[0][j] = dp[1][j];
+            dp[1][j] = 0;
         }
     }
-    return dp[B];
+    return dp[0][B];
 }
