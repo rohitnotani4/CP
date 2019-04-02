dc41898988c43075959f2a53fb9e0c1f91ec2dfb
Rohit Notani <rohitnotani4@gmail.com>
2019-04-02 21:40:53 +0530

    Latest solution. AC on 2nd April 2019.
    Cleaned up the existing code.
    Solution is based on "Largest Rectangle in Histogram" problem.
    Refer : https://leetcode.com/problems/largest-rectangle-in-histogram/

--- InterviewBit/Dynamic Programming/Max Rectangle in Binary Matrix.cpp
+++ InterviewBit/Dynamic Programming/Max Rectangle in Binary Matrix.cpp
@@ -2,75 +2,68 @@
 https://www.interviewbit.com/problems/max-rectangle-in-binary-matrix/
 */
 
-int findMaxArea(vector<int> bars)
+int findMaxArea(vector<int>& bars)
 {
-    stack<int> trackLeft;
-    int top, i = 0, currArea= 0, maxArea=0;
-    while(i < bars.size())
+    int index = 0, maxArea = 0, currentArea = 0;
+    stack<int> trackIndexes;
+    while(index < bars.size())
     {
-        if(trackLeft.empty() || bars[trackLeft.top()] <= bars[i])
+        if (trackIndexes.empty() || bars[trackIndexes.top()] <= bars[index])
         {
-            trackLeft.push(i);
-            i++;
+            trackIndexes.push(index);
+            index++;
         }
         else
         {
-            top = trackLeft.top();
-            trackLeft.pop();
-            
-            if(trackLeft.empty())
-                currArea = bars[top] * i;
-            else    
-                currArea = bars[top] * (i-trackLeft.top()-1); 
-            
-            maxArea = max(currArea,maxArea);    
+            while (!trackIndexes.empty() && bars[trackIndexes.top()] > bars[index])
+            {
+                int popedIndex = trackIndexes.top();
+                trackIndexes.pop();
+                
+                currentArea = trackIndexes.empty()
+                    ? bars[popedIndex] * index
+                    : bars[popedIndex] * (index - 1 - trackIndexes.top());
+                maxArea = max(maxArea, currentArea);
+            }              
         }
     }
     
-    while(!trackLeft.empty())
+    while (!trackIndexes.empty())
     {
-        top = trackLeft.top();
-        trackLeft.pop();
-        
-        if(trackLeft.empty())
-            currArea = bars[top] * i;
-        else    
-            currArea = bars[top] * (i-trackLeft.top()-1); 
-        
-        maxArea = max(currArea,maxArea);    
+        int popedIndex = trackIndexes.top();
+        trackIndexes.pop();
+
+        currentArea = trackIndexes.empty()
+            ? bars[popedIndex] * index
+            : bars[popedIndex] * (index - 1 - trackIndexes.top());
+        maxArea = max(maxArea, currentArea);
     }
     
     return maxArea;
 }
- 
+
 int Solution::maximalRectangle(vector<vector<int> > &A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
     
+    if (A.size() < 1)
+        return 0;
+    
     int maxArea = 0, currArea = 0;
-    vector<int> oneRow;
+    vector<int> oneRow (A[0].size(), 0);
     for(int i=0;i<A.size();i++)
     {
-        if(i==0)
+        for(int j=0;j<A[i].size();j++)
         {
-            oneRow = A[i];
+            if(A[i][j] == 0)
+                oneRow[j] = 0;
+            else
+                oneRow[j] = oneRow[j] + A[i][j];
+            
         }
-        else
-        {
-            for(int j=0;j<A[i].size();j++)
-            {
-                if(A[i][j] == 0)
-                    oneRow[j] = 0;
-                else
-                    oneRow[j] = oneRow[j] + A[i][j];
-                
-                // cout<<oneRow[j]<<" ";
-            }
-        }    
         currArea = findMaxArea(oneRow);
-        // cout<<"Area = "<<currArea<<"\n";
         maxArea = max(currArea,maxArea);      
     }
     return maxArea;
